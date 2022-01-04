b0cfe7feebce7e5f439494241c6dc9ffbaafa3ba
Kuniaki Igarashi <igaiga@gmail.com>
2022-01-04 14:49:47 +0900

    Writing not nil spec/models/book_spec.rb

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Book, type: :model do
   it "trueであるとき、trueになること" do
     expect(true).to eq(true)
   end
+
+  it "Bookモデルをnewしたとき、nilではないこと" do
+    expect(Book.new).not_to eq(nil)
+  end
 end
